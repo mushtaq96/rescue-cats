7812ef6db8d6c91bd85273a625a9035b7f1e77cc
mushtaq96 <mushtaq96smb@gmail.com>
2025-02-07 22:35:10 +0100

    fix: Centralize DbContext creation logic

--- tests/CatRescueApi.Tests/TestFixture.cs
+++ tests/CatRescueApi.Tests/TestFixture.cs
@@ -1,30 +1,19 @@
 using CatRescueApi.Data;
 using Microsoft.EntityFrameworkCore;
-using System;
-using Xunit;
 
 // This is a test fixture, a class that contains shared setup and teardown logic for tests.
-public class TestFixture : IDisposable
+public class TestFixture
 {
-    public ApplicationDbContext DbContext { get; }
-
-    public TestFixture()
+    //  entry point for creating isolated DbContext instances
+    public ApplicationDbContext CreateContext()
     {
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // ensure each test uses a unique database instance
             .Options; // create a new instance of the DbContextOptionsBuilder
 
-        DbContext = new ApplicationDbContext(options); // create a new instance of the ApplicationDbContext
-        DbContext.Database.EnsureCreated(); // create the in-memory database
-    }
-
-    public void Dispose()
-    {
-        // clean up the in-memory database
-        Console.WriteLine("Disposing DB...");
-        DbContext.Database.EnsureDeleted(); // delete the in-memory database
-        DbContext.Dispose(); // dispose of the in-memory database
-        Console.WriteLine("DB disposed");
+        var context = new ApplicationDbContext(options); // create a new instance of the ApplicationDbContext
+        context.Database.EnsureCreated(); // create the in-memory database
+        return context;
     }
 
 }
